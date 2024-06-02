1dd24387548d161b0784344038a95ae863347554
TanKaka123 <hongtan1422002@gmail.com>
2024-06-02 20:08:32 +0700

    add example style category pattern

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
-import {Link, Text, Button } from "@chakra-ui/react";
+import { Link, Text, Button } from "@chakra-ui/react";
 import styles from "./page.module.css";
 import { Language, useLocalization } from "@frontifyHub/common/localization";
+import { ISpacingCategory } from "@frontifyHub/design-component/models";
+import { useEffect, useState } from "react";
+import { SpacingCategory } from "@frontifyHub/design-component/view-models";
 
 export default function Home() {
   const { t, changeLanguage } = useLocalization();
+  const [testSpacingCategory, setTestSpacingCategory] =
+    useState<ISpacingCategory>();
+  useEffect(() => {
+    setTestSpacingCategory(
+      new SpacingCategory().setPadding("10px 20px 30px").setMargin("10px")
+    );
+  }, []);
+  if (testSpacingCategory) {
+    console.log(testSpacingCategory.padding.toString(), testSpacingCategory.margin.toString());
+  }
+
   return (
     <main className={styles.main}>
       {t("home.body")}
