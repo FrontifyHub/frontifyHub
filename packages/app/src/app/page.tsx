f264acbc2590074ac845884de7d4a5f9c77569e5
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-07-05 02:54:01 +0700

    EOD

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,42 +1,22 @@
 "use client";
-import {Link, Text, Button } from "@chakra-ui/react";
+import {
+  Link,
+  Button,
+  ColorModeScript,
+  useColorMode,
+  Box,
+} from "@chakra-ui/react";
 import styles from "./page.module.css";
 import { Language, useLocalization } from "@frontifyHub/common/localization";
-<<<<<<< Updated upstream
-=======
 import frontifyHubTheme from "./theme";
 import { Canvas } from "../../../canvas/src/Canvas";
 import { Header } from "./Header/header";
->>>>>>> Stashed changes
 
 export default function Home() {
-  const { t, changeLanguage } = useLocalization();
   return (
     <main className={styles.main}>
-<<<<<<< Updated upstream
-      {t("home.body")}
-      <button
-        onClick={() => {
-          changeLanguage(Language.Spanish);
-        }}
-      >
-        {t("button.language.spanish")}
-      </button>
-      <button
-        onClick={() => {
-          changeLanguage(Language.English);
-        }}
-      >
-        {t("button.language.english")}
-      </button>
-      <Button bgColor="fron">adasdsadsa</Button>
-
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-=======
       <Canvas />
       <Header />
->>>>>>> Stashed changes
     </main>
   );
 }
---
+++ packages/canvas/src/constant/initializeBlock.ts
@@ -0,0 +1,6 @@
+export const BLOCK_DIVIDER_INITIATION = {
+    width: "1px",
+    height: "300px",
+    padding: '2px',
+    margin: '5px'
+}
