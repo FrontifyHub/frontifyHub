068d5859a9ccb64f7b6e6f25d758e7ce1bf5a679
TanKaka123 <hongtan1422002@gmail.com>
2024-07-10 07:28:31 +0700

    handle click outside toolbox

--- packages/canvas/src/context/BlockSelectingProvider.tsx
+++ packages/canvas/src/context/BlockSelectingProvider.tsx
@@ -4,16 +4,21 @@ import { IBlock, ITreeBlock } from "../block/IBlock";
 type BlockSelectingContext = {
   pathSelectedBlock?: string;
   setPathSelectedBlock: (path: string) => void;
+  onResetSelectedBlock: () => void
 };
 
 const BlockSelectingContext = React.createContext({} as BlockSelectingContext);
 
 export const BlockSelectingProvider = ({ children }: { children: any, treeBlock: ITreeBlock }) => {
-  const [pathSelectedBlock, setPathSelectedBlock] = React.useState<string>()
+  const [pathSelectedBlock, setPathSelectedBlock] = React.useState<string>();
+
+  const onResetSelectedBlock = React.useCallback(() => {
+    setPathSelectedBlock(undefined);
+  }, [setPathSelectedBlock]);
 
   const value = React.useMemo(() => {
-    return { pathSelectedBlock, setPathSelectedBlock };
-  }, [pathSelectedBlock, setPathSelectedBlock]);
+    return { pathSelectedBlock, setPathSelectedBlock, onResetSelectedBlock };
+  }, [pathSelectedBlock, setPathSelectedBlock, onResetSelectedBlock]);
   
   return (
     <BlockSelectingContext.Provider value={value}>
