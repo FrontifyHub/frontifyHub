5357bfb4e678a14a8af87180dde29bc7883de8d4
TanKaka123 <hongtan1422002@gmail.com>
2024-07-16 00:06:52 +0700

    refactoring justify

--- packages/design-component/src/baseStyleCategories/Layout/Justify.ts
+++ packages/design-component/src/baseStyleCategories/Layout/Justify.ts
@@ -6,9 +6,13 @@ export enum EJustifyType {
     JUSTIFY_SELF = 'justifySelf',
 }
 
-export type JustifyContentAttribute = 'normal' | 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch';
-export type JustifyItemsAttribute = 'start' | 'end' | 'center' | 'stretch';
-export type JustifySelfAttribute = 'auto' | 'start' | 'end' | 'center' | 'stretch';
+const justifyContentAttribute = ['normal', 'flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly', 'stretch'] as const;
+const justifyItemsAttribute = ['start', 'end', 'center', 'stretch'] as const;
+const justifySelfAttribute = ['auto', 'start', 'end', 'center', 'stretch'] as const;
+
+export type JustifyContentAttribute = typeof justifyContentAttribute[number];
+export type JustifyItemsAttribute = typeof justifyItemsAttribute[number];
+export type JustifySelfAttribute = typeof justifySelfAttribute[number];
 
 export type JustifyStyle = {
     type: EJustifyType.JUSTIFY_CONTENT,
@@ -42,15 +46,11 @@ export class Justify implements IJustify {
         return this._justify;
     }
     public set justify(value: string) {
-        const justifyContentAttributes: JustifyContentAttribute[] = ['normal', 'flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly', 'stretch'];
-        const justifyItemsAttributes: JustifyItemsAttribute[] = ['start', 'end', 'center', 'stretch'];
-        const justifySelfAttributes: JustifySelfAttribute[] = ['auto', 'start', 'end', 'center', 'stretch'];
-
-        if (this._justify.type === EJustifyType.JUSTIFY_CONTENT && justifyContentAttributes.includes(value as JustifyContentAttribute)) {
+        if (this._justify.type === EJustifyType.JUSTIFY_CONTENT && justifyContentAttribute.includes(value as JustifyContentAttribute)) {
             this._justify.attribute = value as JustifyContentAttribute;
-        } else if (this._justify.type === EJustifyType.JUSTIFY_ITEMS && justifyItemsAttributes.includes(value as JustifyItemsAttribute)) {
+        } else if (this._justify.type === EJustifyType.JUSTIFY_ITEMS && justifyItemsAttribute.includes(value as JustifyItemsAttribute)) {
             this._justify.attribute = value as JustifyItemsAttribute;
-        } else if (this._justify.type === EJustifyType.JUSTIFY_SELF && justifySelfAttributes.includes(value as JustifySelfAttribute)) {
+        } else if (this._justify.type === EJustifyType.JUSTIFY_SELF && justifySelfAttribute.includes(value as JustifySelfAttribute)) {
             this._justify.attribute = value as JustifySelfAttribute;
         } else {
             throw new Error(`Invalid justify attribute value: ${value}`);
