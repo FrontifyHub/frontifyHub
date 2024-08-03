e74448332bc3eeb24f3469a25c1140f28702dde4
TanKaka123 <hongtan1422002@gmail.com>
2024-08-04 00:33:54 +0700

    config sidebar

--- packages/common-ui/src/molecules/NavItem.tsx
+++ packages/common-ui/src/molecules/NavItem.tsx
@@ -1,64 +1,62 @@
 import { Box, ListItem, Text } from '@chakra-ui/react';
-import Link from 'next/link'
+import Link from 'next/link';
 import React from 'react';
 
 type NavItemProps = {
   path: string;
   label?: string;
   icon: React.ReactNode;
-  isSelected: (path: string) => boolean;
-  onSelect: (path: string) => void;
+  isSelected: boolean;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ path, label, icon, isSelected, onSelect }) => (
-  <ListItem
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    position="relative"
-    width="100%"
-    padding="10px"
-    onClick={() => onSelect(path)}
-    cursor="pointer"
-  >
-    {isSelected(path) && (
-      <Box
-        position="absolute"
-        top="50%"
-        left="0"
-        transform="translateY(-50%)"
-        width="3.5px"
-        height="45px"
-        bg="linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)"
-      />
-    )}
-    <Box
-      as="span"
-      bgGradient={isSelected(path) ? "linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)" : "none"}
-      bgClip="text"
-      color={isSelected(path) ? "transparent" : "#A39FA9"}
-      fontSize="24px"
+const NavItem: React.FC<NavItemProps> = ({ path, label, icon, isSelected }) => (
+  <Link href={path}>
+    <ListItem
       display="flex"
-      justifyContent="center"
+      flexDirection="column"
       alignItems="center"
+      position="relative"
+      width="100%"
+      padding="10px"
+      cursor="pointer"
     >
-      {icon}
-    </Box>
-    {label && (
-      <Link
-        // bgGradient={isSelected(path) ? "linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)" : "none"}
-        // bgClip="text"
-        // fontSize="14px"
-        // mt="2px"
-        // textAlign="center"
-        // color={isSelected(path) ? "transparent" : "#A39FA9"}
-
-         href={path}
+      {isSelected ? (
+        <Box
+          position="absolute"
+          top="50%"
+          left="0"
+          transform="translateY(-50%)"
+          width="3.5px"
+          height="45px"
+          bg="linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)"
+        />
+      ) : null}
+      <Box
+        as="span"
+        bgGradient={isSelected ? "linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)" : "none"}
+        bgClip={isSelected ? "text" : "unset"}
+        color={isSelected ? "transparent" : "#A39FA9"}
+        fontSize="24px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
       >
-        {label}
-      </Link>
-    )}
-  </ListItem>
-)
+        {icon}
+      </Box>
+      {label && (
+        <Text
+          bgGradient={isSelected ? "linear-gradient(180deg, #8A46C9 32.5%, #0005FF 100%)" : "none"}
+          bgClip={isSelected ? "text" : "unset"}
+          color={isSelected ? "transparent" : "#A39FA9"}
+          fontSize="14px"
+          mt="2px"
+          textAlign="center"
+        >
+          {label}
+        </Text>
+      )}
+    </ListItem>
+  </Link>
+);
 
 export default NavItem;
